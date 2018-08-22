a0aaa2b04966b176daed2abef3a401114b1639c6
chakannom <genteelson@gmail.com>
2018-08-22 23:48:56 +0900

    진행중

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -2,5 +2,20 @@ var imageManagerApp = new Vue({
     el: '#imageManagerApp',
     data: {
         currentPage: 1
+    },
+    created() {
+        let uri = window.location.href.split('?');
+        if (uri.length == 2) {
+            let vars = uri[1].split('&');
+            let getVars = {};
+            let tmp = '';
+            vars.forEach(function(v){
+                tmp = v.split('=');
+                if(tmp.length == 2)
+                    getVars[tmp[0]] = tmp[1];
+            });
+            console.log(getVars);
+            // do
+        }
     }
 });
