9e7a24c1e87f7ca56150e5d9528c035a071aa6e8
chakannom <genteelson@gmail.com>
2018-08-25 01:12:14 +0900

    작업중

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,21 +1,35 @@
+Vue.prototype.$http = axios;
+
 var imageManagerApp = new Vue({
     el: '#imageManagerApp',
     data: {
-        currentPage: 1
+        imagesFromUpload: [],
+        imagesFromBlog: []
     },
     created() {
-        let uri = window.location.href.split('?');
-        if (uri.length == 2) {
-            let vars = uri[1].split('&');
-            let getVars = {};
+        let imagesFromBlogUrl = 'sample/sample.json';
+        const uri = window.location.href.split('?');
+        if (uri.length > 1) {
+            const vars = uri[1].split('&');
+            const getVars = {};
             let tmp = '';
             vars.forEach(function(v){
                 tmp = v.split('=');
                 if(tmp.length == 2)
                     getVars[tmp[0]] = tmp[1];
             });
-            console.log(getVars);
-            // do
+            if (getVars['iib_u'] !== undefined && getVars['iib_u'] !== '') {
+                imagesFromBlogUrl = getVars['iib_u'];
+            }
+        }
+        this.$http.get(imagesFromBlogUrl).then((response) => {
+            this.imagesFromBlog = response.data;
+        });
+    },
+    methods: {
+        uploadFiles(event) {
+            for (const file of event.target.files) {
+            }
         }
     }
 });
