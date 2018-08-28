5febf462c568c870458f699b166dfd9cad4e2b05
chakannom <genteelson@gmail.com>
2018-08-28 21:53:24 +0900

    클릭시 이미지리스트 가져오도록

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -3,11 +3,12 @@ Vue.prototype.$http = axios;
 var imageManagerApp = new Vue({
     el: '#imageManagerApp',
     data: {
+        imageFromUploadUrl: 'sample/imageFromUpload.json',
         imagesFromUpload: [],
+        imagesFromBlogUrl: 'sample/imagesFromBlog.json',
         imagesFromBlog: []
     },
     created: function() {
-        var imagesFromBlogUrl = 'sample/imagesFromBlog.json';
         var uri = window.location.href.split('?');
         if (uri.length > 1) {
             var vars = uri[1].split('&');
@@ -19,38 +20,46 @@ var imageManagerApp = new Vue({
                     getVars[tmp[0]] = tmp[1];
             });
             if (getVars['iib_u'] !== undefined && getVars['iib_u'] !== '') {
-                imagesFromBlogUrl = getVars['iib_u'];
+                this.imagesFromBlogUrl = getVars['iib_u'];
             }
         }
-        var vm = this;
-        this.$http.get(imagesFromBlogUrl).then(function(response) {
-            vm.imagesFromBlog = response.data;
-        });
     },
     methods: {
         selectFile: function(event) {
             this.$el.querySelector('#select_file').click();
         },
         uploadFiles: function(event) {
-            var imageFromUploadUrl = 'sample/imageFromUpload.json';
             var vm = this;
             for (var i = 0; i < event.target.files.length; i++) {
                 var file = event.target.files[i];
-                console.log(file);
                 var reader = new FileReader();
                 reader.onload = function(event) {
-                    vm.imagesFromUpload.push({id: undefined, src: undefined, file: event.target.result, fid: uuidv4()});
-                    // var formData = new FormData();
-                    // formData.append('uploadFile', file);
-                    // formData.append('filename', file.name);
-                    // vm.$http.post('sample/imageFromUpload' + i + '.json', formData).then(function(response) {
-                    //     image.id = response.data.id;
-                    //     image.url = response.data.url;
-                    //     image.file = undefined;
-                    // });
+                    var fileuuid = uuidv4();
+                    vm.imagesFromUpload.push({id: undefined, src: undefined, file: event.target.result, fid: fileuuid});
+                    var formData = new FormData();
+                    formData.append('uploadFile', file);
+                    formData.append('filename', file.name);
+                    formData.append('fileuuid', fileuuid);
+                    vm.$http.post(vm.imageFromUploadUrl, formData).then(function(response) {
+                        for (var j = 0; j < vm.imagesFromUpload.length; j++) {
+                            if (vm.imagesFromUpload[j].fid === response.config.data.get('fileuuid')) {
+                                vm.imagesFromUpload[j].id = response.data.id;
+                                vm.imagesFromUpload[j].url = response.data.url;
+                                vm.imagesFromUpload[j].file = undefined;
+                                vm.imagesFromUpload[j].fid = undefined;
+                                break;
+                            }
+                        }
+                    });
                 };
                 reader.readAsDataURL(file);
             }
+        },
+        getImagesFromBlog: function(event) {
+            var vm = this;
+            this.$http.get(this.imagesFromBlogUrl).then(function(response) {
+                vm.imagesFromBlog = response.data;
+            });
         }
     }
 });
