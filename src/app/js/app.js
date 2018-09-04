fc92a18b0ad5b58ef8d8dba197df884624db87fd
chakannom <genteelson@gmail.com>
2018-09-04 23:14:31 +0900

    presigned

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -44,7 +44,6 @@ var imageManagerApp = new Vue({
                 }
                 axios.defaults.baseURL = this.baseUrl;
                 var authenticationToken = this.getToken(this.webStorageTokenName);
-                authenticationToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTUzNjA2NTQxNH0.-Fqp0TWI_Xb2Ij4HvDF6ymbbqkr5pv26Ty8lXQCARmNKvr0VGXbTYWil9SMs2tsR0zgedEqfTOQwEU30GiYulg';
                 if (typeof authenticationToken === 'string') {
                     axios.defaults.headers.common['Authorization'] = 'Bearer ' + authenticationToken;
                 }
@@ -57,53 +56,52 @@ var imageManagerApp = new Vue({
             this.$el.querySelector('#select_file').click();
         },
         uploadFiles: function(event) {
-            var files = event.target.files;
             var vm = this;
+            var files = event.target.files;
             for (var i = 0; i < files.length; i++) {
                 var reader = new FileReader();
                 reader.onload = (function(file) {
                     return function(e) {
-                        var images = {id: undefined, url: e.target.result, fid: uuidv4()};
+                        var images = {id: undefined, url: e.target.result, fuid: uuidv4()};
                         vm.imagesFromUpload.push(images);
                         if (!vm.presignedPutUrl) {
-                            vm.uploadFormData(file, images.fid);
+                            vm.uploadFormData(file, images.fuid);
                         } else {
-                            vm.uploadPresignedPutUrl(file, images.fid);
+                            vm.uploadPresignedPutUrl(file, images.fuid);
                         }
                     };
                 })(files[i]);
                 reader.readAsDataURL(files[i]);
             }
-
-            console.log(vm.imagesFromUpload);
         },
-        uploadFormData: function(file, fid) {
+        uploadFormData: function(file, fuid) {
+            var vm = this;
             var formData = new FormData();
             formData.append('uploadFile', file);
             formData.append('filename', file.name);
-            formData.append('fid', fid);
-            axios.post(this.imageFromUploadUrl, formData).then(function (response) {
-                for (var j = 0; j < this.imagesFromUpload.length; j++) {
-                    if (this.imagesFromUpload[j].fid === response.config.data.get('fid')) {
-                        this.imagesFromUpload[j].id = response.data.id;
-                        this.imagesFromUpload[j].url = response.data.url;
-                        this.imagesFromUpload[j].fid = undefined;
+            axios.post(vm.imageFromUploadUrl, formData).then(function (response) {
+                for (var j = 0; j < vm.imagesFromUpload.length; j++) {
+                    if (vm.imagesFromUpload[j].fuid === fuid) {
+                        vm.imagesFromUpload[j].id = response.data.id;
+                        vm.imagesFromUpload[j].url = response.data.url;
+                        vm.imagesFromUpload[j].fuid = undefined;
                         break;
                     }
                 }
             });
         },
-        uploadPresignedPutUrl: function(file, fid) {
+        uploadPresignedPutUrl: function(file, fuid) {
             var vm = this;
-            axios.get(this.presignedPutUrl, { params: { filename: 'user.png' } }).then(function (response) {
+            axios.get(vm.presignedPutUrl, { params: { filename: file.name } }).then(function (response) {
                 axios.put(response.data.url, file).then(function (res) {
-                    console.log(res.config.url.split('?')[0]);
-                    console.log(res);
+                    var imageUrl = res.config.url.split('?')[0];
+                    var tmp = imageUrl.split('/');
+                    var imageId = tmp[tmp.length - 2];
                     for (var j = 0; j < vm.imagesFromUpload.length; j++) {
-                        if (vm.imagesFromUpload[j].fid === res.config.data.get('fid')) {
-                            vm.imagesFromUpload[j].id = res.data.id;
-                            vm.imagesFromUpload[j].url = res.data.url;
-                            vm.imagesFromUpload[j].fid = undefined;
+                        if (vm.imagesFromUpload[j].fuid === fuid) {
+                            vm.imagesFromUpload[j].id = imageId;
+                            vm.imagesFromUpload[j].url = imageUrl;
+                            vm.imagesFromUpload[j].fuid = undefined;
                             break;
                         }
                     }
@@ -112,7 +110,7 @@ var imageManagerApp = new Vue({
         },
         getImagesFromBlog: function(event) {
             var vm = this;
-            axios.get(this.imagesFromBlogUrl).then(function(response) {
+            axios.get(vm.imagesFromBlogUrl).then(function(response) {
                 vm.imagesFromBlog = response.data;
             });
         }
